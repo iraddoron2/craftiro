317f96c22a6e4bf27da5bb20f82f9e32c20a9644
Irad Doron <irad.doron@gmail.com>
2025-01-13 18:44:37 +0200

    add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
+      <h1>
+        Craftiro
+      </h1>
       <main className={styles.main}>
         <Image
           className={styles.logo}
