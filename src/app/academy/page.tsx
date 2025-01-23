67313239a2f155cb2c0d9f398fa6fc00b0ae34ca
Irad Doron <irad.doron@gmail.com>
2025-01-23 18:40:33 +0200

    try to fix bug

--- src/app/academy/page.tsx
+++ src/app/academy/page.tsx
@@ -1,5 +1,57 @@
+'use client'
+
+import { useTabsNavbar } from '@/lib'
+import { LinksGroups } from '@/types'
 import { Stack } from '@core'
+import { PagesNavbar, TabsNavbar } from '@shared'
+import { usePathname } from 'next/navigation'
+import { useEffect, useMemo } from 'react'
 
 export default function Page() {
-    return <Stack>hi</Stack>
+    const pathname = usePathname()
+    const tabsNavbar = useTabsNavbar()
+    const linksGroups: LinksGroups = useMemo(() => [[]], [])
+    useEffect(() => {
+        console.log('linksGroups', linksGroups)
+        console.log('pathname', pathname)
+        console.log('tabsNavbar', tabsNavbar)
+        if (tabsNavbar.currentPath !== pathname) {
+            tabsNavbar.updateCurrentPath(pathname)
+            tabsNavbar.updateLinksGroups(linksGroups)
+        }
+    }, [linksGroups, pathname, tabsNavbar])
+
+    return (
+        <Stack
+            sx={{
+                flexDirection: 'column',
+                minHeight: '100vh',
+            }}
+        >
+            <PagesNavbar
+                links={[
+                    { href: '/academy', label: 'עמוד בית' },
+                    { href: '/academy/learning-diary', label: 'יומן למידה' },
+                    { href: '/academy/syllabus', label: 'סילבוס' },
+                    { href: '/academy/objectives', label: 'משימות' },
+                    { href: '/academy/moduls', label: 'מודולים' },
+                    { href: '/academy/games', label: 'משחקים' },
+                    {
+                        href: '/academy/online-lessons',
+                        label: 'שיעורי אונליין',
+                    },
+                    { href: '/academy/goals', label: 'מטרות' },
+                    { href: '/academy/books', label: 'ספרים' },
+                    { href: '/academy/courses', label: 'קורסים' },
+                    { href: '/academy/blinks', label: 'בלינקס' },
+                    { href: '/academy/certificates', label: 'תעודות' },
+                    { href: '/academy/stats', label: 'סטטיסטיקות' },
+                    { href: '/academy/requests', label: 'בקשות' },
+                    { href: '/academy/plan', label: 'מנוי' },
+                ]}
+            />
+            <Stack>hi</Stack>
+            <TabsNavbar />
+        </Stack>
+    )
 }
