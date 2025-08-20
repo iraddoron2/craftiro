722b9fded980b5ee0bdaee8fff1e90bfdf79ca1d
Irad Doron <irad.doron@gmail.com>
2025-08-20 17:27:42 +0300

    improve design of sign-in page

--- apps/web/src/app/(auth)/sign-in/page.tsx
+++ apps/web/src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,26 @@
+import { Input, MainBackground, SystemHomePageTitle } from '@/components'
 import { login } from '@/lib/auth'
 import { Stack } from '@craftiro/ui'
 import { redirect } from 'next/navigation'
+import { SignInButton } from './_components'
 
 export default async function Page() {
     return (
-        <Stack>
-            <h1>Sign In</h1>
+        <Stack
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '100vh',
+            }}
+        >
+            <SystemHomePageTitle
+                title="כניסה למערכת"
+                subtitle="רשמו את פרטי הכניסה שלכם"
+            />
+            <MainBackground />
+
             <form
                 action={async (formData) => {
                     'use server'
@@ -13,9 +28,40 @@ export default async function Page() {
                     redirect('/') // Redirect to home page
                 }}
             >
-                <input type="email" placeholder="Email" name="email" />
-                <input type="password" placeholder="Password" name="password" />
-                <button type="submit">Sign In</button>
+                <Stack
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        gap: '16px',
+                        width: '100%',
+                        maxWidth: '400px',
+                        marginTop: '80px',
+                    }}
+                >
+                    <Input
+                        type="email"
+                        placeholder="אימייל..."
+                        name="email"
+                        style={{
+                            width: '420px',
+                        }}
+                        autoComplete="email"
+                        required
+                    />
+                    <Input
+                        type="password"
+                        placeholder="סיסמה..."
+                        name="password"
+                        style={{
+                            width: '420px',
+                        }}
+                        autoComplete="current-password"
+                        required
+                    />
+                    <SignInButton />
+                </Stack>
             </form>
         </Stack>
     )
